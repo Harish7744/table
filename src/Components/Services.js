f7398e7e669002bbc4a96d72419f9286872cef47
harish7744 <harishgumpana@gmail.com>
2024-02-10 23:52:17 +0530

    final commit

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -1,37 +1,25 @@
-// import { Carousel } from 'bootstrap'
 import Carousel from 'react-bootstrap/Carousel';
-import React from 'react'
+import React from 'react';
 
 function Services() {
   
-  
     return (
-        <Carousel> 
-      <Carousel.Item>
-      <img src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/portfolio-testimonials-1-img.png" class="testimonial-card-image" alt="..." />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-      <img src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/portfolio-testimonials-1-img.png" class="testimonial-card-image" alt="..." />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-      <img src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/portfolio-testimonials-1-img.png" class="testimonial-card-image" alt="..." />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-  )
+      <>
+        <Carousel className='Courisel' style={{marginTop:30}}> 
+          <Carousel.Item className="text-center">
+            <img src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/portfolio-testimonials-1-img.png" class="testimonial-card-image" alt="..." />
+          </Carousel.Item>
+          <Carousel.Item className="text-center">
+            <img src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/portfolio-testimonials-1-img.png" class="testimonial-card-image" alt="..." />
+          </Carousel.Item>
+          <Carousel.Item className="text-center">
+            <img src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/portfolio-testimonials-1-img.png" class="testimonial-card-image" alt="..." />
+          </Carousel.Item>
+        </Carousel>
+        <h1>Harish</h1>
+        <p>ugqgyus uxgugsxyg ygxasgx</p>
+      </>
+  );
 }
 
-export default Services
+export default Services;
